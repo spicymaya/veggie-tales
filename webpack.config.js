79490e520073420d8d2f850992a8bf44caefec24
Maya Davis <kryvitskaya@gmail.com>
2019-03-01 17:08:26 -0500

    Add SCSS and needed dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: ["@babel/polyfill", "./src/index.js"],
+  output: {
+    publicPath: "/"
+  },
   module: {
     rules: [
       {
@@ -11,19 +14,35 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
+        exclude: /node_modules/,
         use: [
           "style-loader",
           {
             loader: "css-loader",
             options: {
-              modules: true,
+              // modules: true,
               localIdentName: "[path][name]__[local]--[hash:base64:5]"
             }
+          },
+          {
+            loader: "postcss-loader", // Run post css actions
+            options: {
+              plugins: function() {
+                // post css plugins, can be exported to postcss.config.js
+                return [require("precss"), require("autoprefixer")];
+              }
+            }
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
           }
         ]
       }
     ]
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebPackPlugin({
