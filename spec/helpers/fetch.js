a3a3db85ea8817e6170f4e404c00833c3fba698c
Maya Davis <kryvitskaya@gmail.com>
2019-03-05 16:25:59 -0500

    correct config for unit test fetch 🙌🏻

--- spec/helpers/fetch.js
+++ spec/helpers/fetch.js
@@ -1,12 +1,1 @@
-// import 'whatwg-fetch';
-
-// const { Response, Request, Headers, fetch } = require('whatwg-fetch');
-// global.Response = Response;
-// global.Request = Request;
-// global.Headers = Headers;
-// global.fetch = fetch;
-
-// import { fetch as fetchPolyfill } from 'whatwg-fetch';
-// glocal.fetch = fetchPolyfill.fetch;
-
-global.fetch = () => Promise.resolve({ json: () => [] });
+global.fetch = () => Promise.resolve({ json: () => null });
