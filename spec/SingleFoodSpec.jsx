3c38b3497893b3ccbe442aa65395b08b61f9e061
Maya Davis <kryvitskaya@gmail.com>
2019-03-05 16:36:16 -0500

    Clean up unused dependencies

--- spec/SingleFoodSpec.jsx
+++ spec/SingleFoodSpec.jsx
@@ -1,36 +1,22 @@
 import React from "react";
 import "@babel/polyfill";
-import { mount, shallow } from "enzyme";
-import * as fetch from "../lib/fetch.js";
+import { mount } from "enzyme";
 import SingleFood from "../src/js/components/SingleFood";
 describe("SingleFood", () => {
-  it("should render Avocado", function(done) {
+  it("should render Avocado", function() {
     const singleFoodData = {
       id: 1,
       name: "Avocado",
       region: "Peru",
       type: "Vegetable",
-      rating: 8,
-      image_url: null,
-      created_at: null,
-      updated_at: null
+      rating: 8
     };
-    const spy = spyOn(fetch, "getSingleFood").and.returnValue(
-      Promise.resolve(singleFoodData)
-    );
-    const wrapper = mount(<SingleFood match={{ params: { id: 1 } }} />);
-    fetch.getSingleFood().then(function(result) {
-      expect(result).toEqual(singleFoodData);
-    });
-    spy.calls.mostRecent().returnValue.then(() => {
-      expect(spy).toHaveBeenCalled();
-      expect(
-        wrapper
-          .find(".qa-name")
-          .hostNodes()
-          .text()
-      ).toContain("Avocado");
-      done();
-    });
+    const wrapper = mount(<SingleFood data={singleFoodData} />);
+    expect(
+      wrapper
+        .find(".qa-name")
+        .hostNodes()
+        .text()
+    ).toContain("Avocado");
   });
 });
