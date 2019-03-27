9399488d8d410fb46cfc580872efd5fcf4c2f96b
Maya Davis <kryvitskaya@gmail.com>
2019-03-27 17:44:33 -0400

    Rename app to server

---
+++ server.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV === "production";
+const path = require("path");
+const express = require("express");
+process.env.NODE_ENV = "production";
+// import path from "path";
+// import express from "express";
+const app = express(),
+  DIST_DIR = __dirname,
+  HTML_FILE = path.join(DIST_DIR, "/build/index.html");
+app.use(express.static(path.join(__dirname, "/build")));
+app.get("*", (req, res) => {
+  res.sendFile(HTML_FILE);
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(
+    `Mode is set to ${process.env.NODE_ENV}`,
+    `App listening to ${PORT}....`
+  );
+});
