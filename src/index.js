023a4954f89c66366c3e3f86f9ee3c0e6bf134da
Maya Davis <kryvitskaya@gmail.com>
2019-03-27 17:49:09 -0400

    Add bootstrap in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/app.scss";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import App from "./js/components/App.jsx";
 //if I want RenderDOM to be conditional
 // const wrapper = document.getElementById("veggie-tales");
