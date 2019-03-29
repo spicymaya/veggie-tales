3f7117122ff81de66da30132b2c5cc5f5a553963
Maya Davis <kryvitskaya@gmail.com>
2019-03-29 18:31:25 -0400

    Move babel config to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,11 @@ const config = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
         }
       },
       {
