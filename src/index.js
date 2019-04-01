9dcf1a59d4ce40afd80a5c6a17d8d924b8aa088a
Maya Davis <kryvitskaya@gmail.com>
2019-04-01 15:01:07 -0400

    Move bootstrap to html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/app.scss";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from "./js/components/App.jsx";
 //if I want RenderDOM to be conditional
 // const wrapper = document.getElementById("veggie-tales");
