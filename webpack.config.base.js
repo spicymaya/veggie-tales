fa6195b6e8194acd1dd77a613567d2cdfcb6eea4
Maya Davis <kryvitskaya@gmail.com>
2019-04-01 15:00:03 -0400

    Add react hot plugin

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -15,7 +15,10 @@ module.exports = {
         loader: "babel-loader",
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-proposal-class-properties"]
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-hot-loader/babel"
+          ]
         }
       },
       {
