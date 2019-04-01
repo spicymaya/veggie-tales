ff9dfa5825f8cb4ae83ec342778b0c29cf54fdb8
Maya Davis <kryvitskaya@gmail.com>
2019-04-01 14:59:51 -0400

    Add bundle analyzer

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,17 @@
 const merge = require("webpack-merge");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const baseConfig = require("./webpack.config.base.js");
 
 module.exports = merge(baseConfig, {
-  mode: "production"
+  mode: "production",
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      reportFilename: "bundle_sizer.html"
+    })
+  ],
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM"
+  }
 });
