9fdeeb28c9badf9a48dd805c4a216fbde254fc81
Maya Davis <kryvitskaya@gmail.com>
2019-04-02 16:35:19 -0400

    Remove console.log(eslint)

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.get("*", (req, res) => {
 });
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(
-    `Mode is set to ${process.env.NODE_ENV}`,
-    `App listening to ${PORT}....`
-  );
+  // console.log(
+  //   `Mode is set to ${process.env.NODE_ENV}`,
+  //   `App listening to ${PORT}....`
+  // );
 });
